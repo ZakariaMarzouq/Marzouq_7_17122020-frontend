bacc12ac4c864fb02f948d17657f8a5b0928c6f9
ZakariaMarzouq <76443610+ZakariaMarzouq@users.noreply.github.com>
2021-08-19 12:27:01 +0200

    Creation des components login et register

--- src/components/Log/index.js
+++ src/components/Log/index.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import SignUpForm from './SignUpForm';
+import SignInForm from './SignInForm';
 
-const index = () => {
+const Log = () => {
+  const [signUpModal, setSignUpModal] = useState(true);
+  const [signInModal, setSignInModal] = useState(false);
+
+  const handleModals = (e) => {
+    if (e.target.id === "register") {
+      setSignInModal(false);
+      setSignUpModal(true);
+    } else if (e.target.id === "login") {
+      setSignUpModal(false);
+      setSignInModal(true);
+    }
+  }
+
   return (
     <div className="connection-from">
-
       <div className="form-container">
+        <ul>
+          <li onClick={handleModals} id="register">S'inscrire</li>
+          <li onClick={handleModals} id="login">Se connecter</li>
+        </ul>
+        {signUpModal && <SignUpForm />}
+        {signInModal && <SignInForm/>}
 
-      </div>   
-     
+      </div>     
     </div>
   );
 };
 
-export default index;
+export default Log;
