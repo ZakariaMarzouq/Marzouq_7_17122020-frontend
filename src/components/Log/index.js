593df665817f9f05f72a6813eaa8f59b2120038c
ZakariaMarzouq <76443610+ZakariaMarzouq@users.noreply.github.com>
2021-08-19 11:55:06 +0200

    Création du front-end

---
+++ src/components/Log/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const index = () => {
+  return (
+    <div className="connection-from">
+
+      <div className="form-container">
+
+      </div>   
+     
+    </div>
+  );
+};
+
+export default index;
---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      Hello depuis la Home Page !
+    </div>
+  );
+};
+
+export default Home;
